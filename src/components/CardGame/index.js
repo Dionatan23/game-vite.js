ba93fe38e48ce7e26618a9808613229fa7764a91
Dionatan23 <alvestavares1533@gmail.com>
2023-06-03 19:19:21 -0300

    Inicio do projeto Game com Vite.JS

---
+++ src/components/CardGame/index.js
@@ -0,0 +1,11 @@
+import './style.css';
+
+const CardGame = () => {
+    return `
+        <article class="card-game">
+            <img src="images/alura-pixel 1.png" alt="Logo da alura">
+        </article>
+    `
+};
+
+export default CardGame;
