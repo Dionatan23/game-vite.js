9973a65884567d1319b6661069d025825be12937
Dionatan23 <alvestavares1533@gmail.com>
2023-07-10 17:13:44 -0300

    :lipstick: Criacão do component ScoreBoard

---
+++ src/objects/ScoreBoard/index.js
@@ -0,0 +1,12 @@
+import PlayerName from "../../components/PlayerName";
+
+const ScoreBoard = () => {
+    return /*html*/`
+        <header class="score-board">
+            ${PlayerName("Player 1")}
+            ${PlayerName("Player 2")}
+        </header>
+    `;
+};
+
+export default ScoreBoard;
