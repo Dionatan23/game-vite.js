e5e1f3703ffd51e13fd9ad2afe181670e2880029
Dionatan23 <alvestavares1533@gmail.com>
2023-07-21 16:29:07 -0300

    :construction: Criação da estrutura do CardFrontBack, add image

--- src/components/CardGame/index.js
+++ src/components/CardGame/index.js
@@ -1,11 +1,11 @@
-import './style.css';
+import "./style.css";
 
-const CardGame = () => {
-    return /*html*/`
+const CardGame = (icon = "alura-pixel", alt = "Logo da alura") => {
+  return /*html*/ `
         <article class="card-game">
-            <img src="images/alura-pixel 1.png" alt="Logo da alura">
+            <img src="images/${icon}.png" alt="${alt}">
         </article>
-    `
+    `;
 };
 
 export default CardGame;
