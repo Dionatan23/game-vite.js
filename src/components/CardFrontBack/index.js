481b590e0e78b80d9e4cf2083edfc9c75352aee9
Dionatan23 <alvestavares1533@gmail.com>
2023-07-23 10:34:33 -0300

    :construction: Ajuste e atualização no cards front back

--- src/components/CardFrontBack/index.js
+++ src/components/CardFrontBack/index.js
@@ -1,10 +1,15 @@
 import CardGame from '../CardGame';
+import "./style.css";
 
 const CardFrontBack = () => {
     return /*html*/`
         <article class="card-front-back">
-            ${CardGame()}
-            ${CardGame("javascript", "logo do js")}
+            <div class="card -front">
+                ${CardGame()}
+            </div>
+            <div class="card -back">
+                ${CardGame("javascript", "logo do js")}
+            </div>
         </article>
     `
 };
